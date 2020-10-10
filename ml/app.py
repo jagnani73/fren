a96c9cb8b438d2e7c81fbffb077e577d98ec5332
saxenabhishek <as7122000@gmail.com>
2020-10-11 02:30:25 +0530

    chore(senti): modified helper to work for senti

--- ml/app.py
+++ ml/app.py
@@ -9,6 +9,7 @@ Internal api
 from flask import Flask,render_template,request,redirect,url_for,Response
 import json
 import senti as S
+import tfidf 
 
 app = Flask(__name__)
 
@@ -24,12 +25,15 @@ def login():
    #return Response(json.dumps(user,indent=2),mimetype='application/json')
    return Response(json.dumps(user),mimetype='application/json')
 
-def helper(post,fun):
-   A = fun(post["log"])
-   temp = dict(post)
-   del temp["log"]
-   temp["log"] = A
+def senti_helper(post,fun):
+   A = S.sentiment_analysis(post["log"])
+   temp = {
+      "noteId":post["_id"],
+      "time":post["date"],
+      "score":A
+   }
    return temp
+
 
 if __name__ == '__main__':
    #app.run(host="0.0.0.0", port=8080,debug=True)
@@ -38,7 +42,13 @@ if __name__ == '__main__':
           "userId": "5f69dc132b94c92da8f1785f",
           "log": "Maine aaj dhoka khaya.",
           "date": 1602307428860,
-        }
+   }
+   T = {
+         "_id": "5f69dc5408009d1718b25edf",
+          "userId": "5f69dc132b94c92da8f1785f",
+          "log": "Mera roz kat ta hai guys",
+          "date": 1602307428860,
+   }
    
-   print(helper(R,S.sentiment_analysis))
+   print(helper(T,tfidf.tfidf))
    print(R)
