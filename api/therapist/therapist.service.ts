f7935431ef7f009bb2f6c4a7ed70bbdc7eeb1627
Gita Alekhya Paul <gitaalekhyapaul@gmail.com>
2020-10-11 09:00:34 +0530

    feat(server/therapist): add services

--- api/therapist/therapist.service.ts
+++ api/therapist/therapist.service.ts
@@ -1,8 +1,14 @@
+import * as MongoDB from "mongodb";
+
 import { DatabaseService } from "../services/database.service";
 import { jwtPayload } from "../util/validateJwt";
 import { userDBSchema } from "../auth/auth.schema";
 import { errors } from "../error/error.constant";
-import { getClientsResponse } from "./therapist.schema";
+import {
+  getClientsResponse,
+  noteFetchSchema,
+  pythonRequestSchema,
+} from "./therapist.schema";
 
 export const getClients = async (
   userDetails: jwtPayload
@@ -21,6 +27,7 @@ export const getClients = async (
       },
       {
         $project: {
+          "patients._id": 1,
           "patients.name": 1,
           "patients.email": 1,
         },
@@ -29,4 +36,41 @@ export const getClients = async (
     .toArray();
   if (findClients.length === 0) throw errors.NOT_FOUND;
   return findClients[0];
+};
+
+export const generateReport = async (
+  therapistDetails: jwtPayload,
+  userId: string
+) => {
+  const userDb = await DatabaseService.getInstance().getCollection("users");
+  const relationExists = await userDb.findOne<userDBSchema>({
+    email: therapistDetails.email,
+    category: "therapist",
+    clients: new MongoDB.ObjectID(userId),
+  });
+  if (!relationExists) throw errors.RELATION_NOT_FOUND;
+  const notesDb = await DatabaseService.getInstance().getCollection("notes");
+  const notes = await notesDb
+    .aggregate<noteFetchSchema>([
+      {
+        $match: { userId: new MongoDB.ObjectID(userId), isAnalysed: false },
+      },
+      {
+        $project: { isAnalysed: 0, title: 0 },
+      },
+    ])
+    .toArray();
+  const result = await notesDb.updateMany(
+    { userId: new MongoDB.ObjectID(userId), isAnalysed: false },
+    {
+      $set: { isAnalysed: true },
+    }
+  );
+  if (result.modifiedCount !== notes.length) throw errors.MONGODB_QUERY_ERROR;
+  const pythonRequest: pythonRequestSchema = {
+    notes: notes,
+    userId: userId,
+    therapistId: relationExists._id?.toHexString()!,
+  };
+  console.log("%o", pythonRequest);
 };
